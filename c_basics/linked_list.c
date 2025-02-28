5a4a5cb76357abe475669d2cd6e1fda86ea556eb
Nickson Kaigi <nickaigi@gmail.com>
2025-02-28 20:58:10 +0300

    added more LL functions

--- c_basics/linked_list.c
+++ c_basics/linked_list.c
@@ -26,15 +26,41 @@ node_t *create_new_node(int value){
   return result;
 }
 
+node_t *insert_at_head(node_t **head, node_t *node_to_insert){
+  node_to_insert->next = *head;
+  *head = node_to_insert;
+  return node_to_insert;
+}
+
+node_t *find_node(node_t *head, int value){
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    if (tmp->value == value){
+      return tmp;
+    }
+    tmp = tmp->next;
+  }
+  return NULL;
+}
+
+void insert_after_node(node_t *node_to_insert_after, node_t *new_node){
+  new_node->next = node_to_insert_after->next;
+  node_to_insert_after->next = new_node;
+}
+
 int main(int argc, char **argv){
   node_t *head = NULL;
   node_t *tmp;
 
   for (int i = 0; i < 20; i++){
     tmp = create_new_node(i);
-    tmp->next = head;
-    head = tmp;
+    insert_at_head(&head, tmp);
   }
+
+  tmp = find_node(head, 13);
+  printf("Found node with value %d\n", tmp->value);
+  // insert new node after node 13
+  insert_after_node(tmp, create_new_node(75));
   printList(head);
   return 0;
 }
